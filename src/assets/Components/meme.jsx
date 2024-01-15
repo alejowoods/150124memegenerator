f21d525614a6d1ffd11d735827ef9ff613cccd04
Moustafa Nady <mostafanadii93@gmail.com>
2024-01-15 13:52:24 +0100

    meme fixing

--- src/assets/Components/meme.jsx
+++ src/assets/Components/meme.jsx
@@ -1,35 +1,28 @@
-import React, { useState } from "react";
-    
-const [allMemes, setAllMemes] = useState([]);
+import React, { useState, useEffect } from "react";
 
-    React.useEffect(() => {
-        async function fetchData() {
-          const res = await fetch("https://api.imgflip.com/");
-          const data = await res.json();
-          setAllMemes(data.data.memes);
-        }
-        fetchData();
-      }, []);
+const Meme = () => {
+  const [allMemes, setAllMemes] = useState([]);
 
-      function getMemeImage() {
-        var meme1 = allMemes;
-        meme1 = meme1[Math.floor(Math.random() * meme1.length)];
-        meme1 = meme1.url;
-        return meme1;
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setAllMemes(data.data.memes);
+    }
+    fetchData();
+  }, []);
 
-      }
-    
-      const [memeImage, setmemeImage] = useState({
-        topText: "",
-        bottomText: "",
-        image: "http://i.imgflip.com/1bij.jpg",
-      });
+  function getMemeImage() {
+    if (allMemes.length === 0) return ""; // return early if no memes have been fetched
+    const meme = allMemes[Math.floor(Math.random() * allMemes.length)];
+    return meme.url;
+  }
 
-      function getMemeImage() {
-        var meme1 = allMemes;
-        meme1 = meme1[Math.floor(Math.random() * meme1.length)];
-        meme1 = meme1.url;
-        return meme1;
-      }
+  return (
+    <div>
+      <img src={getMemeImage()} alt="Random Meme" />
+    </div>
+  );
+};
 
-export default meme;
+export default Meme;
