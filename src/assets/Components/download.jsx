74e53a8b9f63af2365802ba9fdb8161e0a474fd2
Moustafa Nady <mostafanadii93@gmail.com>
2024-01-15 15:13:05 +0100

    Buttons and download files

--- src/assets/Components/download.jsx
+++ src/assets/Components/download.jsx
@@ -1,3 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+function Download() {
+  const [allMemes, setAllMemes] = useState([]);
+  const [currentMemeIndex, setCurrentMemeIndex] = useState(0);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setAllMemes(data.data.memes);
+    }
+    fetchData();
+  }, []);
+
+  if (allMemes.length === 0) return null;
+
+  const currentMeme = allMemes[currentMemeIndex];
 
   return (
     <div>
@@ -6,5 +24,6 @@
       </a>
     </div>
   );
+}
 
 export default Download;
