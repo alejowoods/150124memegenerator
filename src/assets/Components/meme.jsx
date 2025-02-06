4ca6b06bee0bf580a016c95b5e73f1e983ee4823
Alejandro Moreno <alejowoods@gmail.com>
2025-02-06 22:20:48 +0100

    new commit

--- src/assets/Components/meme.jsx
+++ src/assets/Components/meme.jsx
@@ -1,66 +1,41 @@
-import React, { useState, useEffect } from "react";
-import OnClickMessage from './OnClickMessage';
-import UploadPic from './UploadPic';
-import '../Styles/meme.css'; 
-
+import React, { useState, useEffect } from 'react';
 
 const Meme = () => {
-  const [allMemes, setAllMemes] = useState([]);
-  const [memeImage, setMemeImage] = useState("");
-  const [error, setError] = useState(null); 
-  const [uploadedImage, setUploadedImage] = useState('');
+    const [image, setImage] = useState('');
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await fetch("https://api.imgflip.com/get_memes");
-        if(!res.ok) {
-          throw Error(res.statusText); 
-        }
-        const data = await res.json();
-        console.log(data); 
-        setAllMemes(data.data.memes);
-      } catch (error) {
-        setError(error.message) 
-      } finally {
-        console.log("Petttion finished");
-      }
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = () => {
+            setImage(reader.result);
+        };
+        reader.readAsDataURL(file);
+    };
 
-    }
-    fetchData(); 
-  }, []); 
+    useEffect(() => {
+        const loadImage = async () => {
+            try {
+                const response = await fetch('https://api.imgflip.com/get_memes');
+                const data = await response.json();
+                setImage(data.data.memes[0].url);
+                console.log(data.data.memes[0].url);
+            } catch (error) {
+                console.log(error)
+            } finally {
+                console.log('promised ended') // add setLoading(false);
+            }
+        };
+        loadImage();
+    }, []);
 
-  useEffect(() => { 
-    if (Array.isArray(allMemes) && allMemes.length > 0 && !uploadedImage) {
-      const meme = allMemes[Math.floor(Math.random() * allMemes.length)];
-      setMemeImage(meme.url); 
-    }
-  }, [allMemes, uploadedImage]); // necesitamos las dos dependencias para que se ejecute el useEffect cuando cambie allMemes o uploadedImage
-
-  if (error) {
-    return <div>Error: {error}</div> 
-
-  }
-
-  return (
-
-    <div>
-      {error && <p>{error}</p>}
-      {uploadedImage ? (
-        <div className="meme-image">
-          <img src={uploadedImage} alt="Uploaded Meme" />
-          <OnClickMessage memeImage={uploadedImage} />
+    return (
+        <>
+        <div>
+            <input type='file' accept='image/*' onChange={handleFileChange}></input>
+            {image && <img src={image} alt="meme" />}
         </div>
-      ) : (
-        Array.isArray(allMemes) && allMemes.length > 0 && (
-          <div className="meme-image" >
-            <OnClickMessage memeImage={memeImage} />
-          </div>
-        )
-      )}
-    </div>
-
-  );
+        </>
+    );
 };
 
 export default Meme;
